b5a2f2acbf2fcd37b77472093023cc2f9922ee89
rzatalibov555 <sethubpro@gmail.com>
2023-03-31 22:10:17 +0400

    filter category, min price , max price

--- products/admin.py
+++ products/admin.py
@@ -1,15 +1,20 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
+import products.models
 # Register your models here.
 from .models import Category, Product, ProductImage
 from .models import *
 
 class ProductsAdmin(admin.ModelAdmin):
-    list_display = ('id','name','category','price','discount_persentage','discount_price','created_at')
+    list_display = ('id','name','category','price','discount_persentage','discount_price','in_sale','created_at', "first_image")
     list_display_links = ('id','name')
     search_fields = ("name","description")
     # list_editable = ('in_sale',)
     list_filter = ('created_at','category')
+
+    def first_image(self, obj):
+        return mark_safe(f"<a target='_blank' href='{obj.productimage_set.first().image.url}'>{obj.productimage_set.first().image.url}</a>") if obj.productimage_set.exists() else None
 
 
 class CategoryAdmin(admin.ModelAdmin):
@@ -18,19 +23,17 @@ class CategoryAdmin(admin.ModelAdmin):
     search_fields = ("name",)
 
 
-class ProductImageAdmin(admin.TabularInline):
+class ProductImageAdmin(admin.TabularInline): #admin panelde  productun icine salsin deye TabularInline istifade olunur
     model = ProductImage
     extra = 1
-
 
 
 class ProductAdmin(admin.ModelAdmin):
     class Meta:
         model = Product
-
     inlines = (ProductImageAdmin,)
 
 
-admin.site.register(Category,CategoryAdmin)
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Product, ProductsAdmin)
 admin.site.register(ProductImage,)
--- products/views.py
+++ products/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Category, Product
-from django.db.models import F, FloatField
+from django.db.models import F, FloatField, Q
 from django.db.models.functions import Coalesce
 
 def index_view(request):
@@ -49,12 +49,42 @@ def product_detail_view(request, id):
 def product_list_view(request):
 
     products = Product.objects.annotate(
-        discount=Coalesce("discount_price", 0, output_field=FloatField())
+        discount=Coalesce("discount_price", 0, output_field=FloatField()),
+        sorting_price=Coalesce("discount_price", "price", output_field=FloatField())
     ).annotate(
-        total_price=F("price") - F("discount_price")
+        # total_price=F("price") - F("discount_price")
+        total_price=F("price") - F("discount")
     ).order_by("-created_at")
 
+    # print(products.values("price","discount_price","sorting_price"))
+
     categories = Category.objects.filter(parent__isnull=True)
+
+
+    category = request.GET.get("category", None)
+    # print(f"Category: {category}")
+    if category:
+        products = products.filter(
+            Q(category_id=int(category)) |
+            Q(category__parent__id=int(category)) |
+            Q(category__parent__parent__id=int(category))
+        )
+
+    min_price = request.GET.get("min_price", None)
+    max_price = request.GET.get("max_price", None)
+
+    if min_price:
+        products = products.filter(sorting_price__gte=min_price)
+
+        # products = products.filter(
+        #     Q(discount_price__gte=min_price) |
+        #     Q(price__gte=min_price)
+        # )
+
+    if max_price:
+        products = products.filter(sorting_price__lte=max_price)
+
+
 
     context = {
         "products": products,
